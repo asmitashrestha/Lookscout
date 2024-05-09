58f6d906e391da933e26acd1674c4dabe3c0e8d2
Your Name <asmitastha2001@gmail.com>
2024-05-09 19:13:43 +0545

    feat demonstartion page added

--- frontend/src/components/ExploreMore.jsx
+++ frontend/src/components/ExploreMore.jsx
@@ -6,11 +6,11 @@ const ExploreMore = () => {
     <div className="px-24 pt-16">
       <div className="flex justify-between flex-wrap">
         <div>
-          <div className="">
+          <div className="font-bold text-gray-900 text-2xl">
             <p>Demonstrate branding</p>
             <p>consequently think outside</p>
           </div>
-          <div className="">
+          <div className="my-4 text-gray-600 text-[16px] font-semibold">
             <p>
               Velit purus egestas tellus phasellus, Mattis eget sed faucibus
               magna
@@ -23,28 +23,28 @@ const ExploreMore = () => {
           <div className="">
             <div className="flex">
               <p className="mt-1">
-                <TiTick />
+                <TiTick className="text-blue-600 text-xl"/>
               </p>
-              <p>Enterprise-grade security</p>
+              <p className="text-gray-600 text-[16px] mb-2">Enterprise-grade security</p>
             </div>
             <div className="flex">
               <p className="mt-1">
-                <TiTick />
+                <TiTick className="text-blue-600 text-xl"/>
               </p>
-              <p>99.9% guaranted uptime SLA</p>
+              <p className="text-gray-600 text-[16px] mb-2">99.9% guaranted uptime SLA</p>
             </div>
             <div className="flex">
               <p className="mt-1">
-                <TiTick />
+                <TiTick className="text-blue-600 text-xl"/>
               </p>
-              <p>Designated customer success team</p>
+              <p className="text-gray-600 text-[16px] mb-2">Designated customer success team</p>
             </div>
           </div>
         </div>
 
         <div className="mr-12">
           <div>
-            <img src={back} alt="img" />
+            <img src={back} alt="img" height={560} className="img"/>
           </div>
           <div className="bg-white rounded-lg relative bottom-[70px]">
               <p>Check Us Out</p>
