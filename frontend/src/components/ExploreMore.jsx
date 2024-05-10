04c2aea79287909a1e069e799e3381236d2bcb84
Your Name <asmitastha2001@gmail.com>
2024-05-10 06:50:43 +0545

    home page changes

--- frontend/src/components/ExploreMore.jsx
+++ frontend/src/components/ExploreMore.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TiTick } from "react-icons/ti";
 import back from "../assets/back.jpeg";
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 const ExploreMore = () => {
   return (
     <div className="px-24 pt-16">
@@ -46,9 +48,16 @@ const ExploreMore = () => {
           <div>
             <img src={back} alt="img" height={560} className="img"/>
           </div>
-          <div className="bg-white rounded-lg relative bottom-[70px]">
-              <p>Check Us Out</p>
-              <button>Get Started Now</button>
+          <div className="bg-white rounded-lg relative bottom-[180px] mx-5 h-[150px]">
+            <div className="">
+                <p className="flex justify-center p-5 text-2xl font-bold">Check Us Out</p>
+              <hr className="border-t-4 border-gray-900 h-30 my-8"/>
+              <div className="flex justify-center">
+                <button className="bg-blue-600 px-7 md:px-10 py-2 rounded">Get Started Now</button>
+              </div>
+              
+            </div>
+            
           </div>
         </div>
 
